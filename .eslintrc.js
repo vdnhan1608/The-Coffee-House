87dbc96559f58c704f24e63a0f97c297363aa6a7
bunrealcua <voduynhan.123@gmail.com>
2023-07-10 15:37:45 +0700

    setup react native templatge

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+	'eslint.workingDirectories': [
+		{ directory: '/TheCoffeeHouse', changeProcessCWD: true },
+	],
+	parser: '@babel/eslint-parser',
+	parserOptions: {
+		requireConfigFile: false,
+	},
+};
